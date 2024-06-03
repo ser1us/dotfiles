4ffe26010cc5f7ae8c71f93e5a1f4d524da5bc6b
Christoph Lipp <christoph@lipp.ws>
2024-06-03 09:53:07 +0200

    done

---
+++ home/.local/bin/cht.sh
@@ -0,0 +1,16 @@
+## Taken from https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/cht.sh
+
+#!/usr/bin/env bash
+selected=`cat ~/.cht-languages ~/.cht-command | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+    bash -c "curl -s cht.sh/$selected~$query | less"
+fi
