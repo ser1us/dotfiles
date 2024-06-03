dc705dc43d13604e0635e599ba43dc44a5d6ecc9
Christoph Lipp <christoph@lipp.ws>
2024-06-03 07:58:34 +0000

    added less

--- home/.local/bin/cht.sh
+++ home/.local/bin/cht.sh
@@ -10,7 +10,7 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query | less"
 else
     bash -c "curl -s cht.sh/$selected~$query | less"
 fi
