bbba38b962b87108cf2d8e6f4c4abafbe16d2087
Christoph Lipp <christoph@lipp.ws>
2024-12-04 22:25:04 +0100

    updated extras
    added smear, added more themes, still unsure :D

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -2,12 +2,26 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,
+    -- priority = 1000,
+  },
+  {
+    "shaunsingh/nord.nvim",
+    name = "nord",
+  },
+  {
+    "sainnhe/everforest",
+    name = "everforest",
+  },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
   },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-latte",
+      -- colorscheme = "nord",
+      -- colorscheme = "tokyonight-day",
     },
   },
 }
