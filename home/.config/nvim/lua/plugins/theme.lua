96cb3e05edb0f86f156caa3c8e91db9eb5740fbb
Christoph Lipp <christoph@lipp.ws>
2024-12-14 16:16:28 +0100

    switching colorschema

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -2,7 +2,6 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    -- priority = 1000,
   },
   {
     "shaunsingh/nord.nvim",
@@ -19,7 +18,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-latte",
+      colorscheme = "catppuccin",
       -- colorscheme = "nord",
       -- colorscheme = "tokyonight-day",
     },
