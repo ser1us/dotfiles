93a8ff5b08f9838f6e0efee5b397bada2e1116f5
Christoph Lipp <christoph@lipp.ws>
2025-02-24 08:56:56 +0100

    removed leap, just use flash honestly

--- home/config/nvim/lua/plugins/theme.lua
+++ home/config/nvim/lua/plugins/theme.lua
@@ -20,7 +20,7 @@ return {
     opts = {
       -- colorscheme = "catppuccin",
       -- colorscheme = "nord",
-      colorscheme = "tokyonight-day",
+      -- colorscheme = "tokyonight-day",
       -- colorscheme = "tokyonight",
     },
   },
