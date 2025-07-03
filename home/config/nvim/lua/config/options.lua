1bd298ee3c168666925a93b5c3f35f355323da22
Christoph Lipp <christoph@lipp.ws>
2025-07-03 13:48:13 +0200

    added whitespace characters

--- home/config/nvim/lua/config/options.lua
+++ home/config/nvim/lua/config/options.lua
@@ -2,4 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.o.list = true
+vim.o.listchars = "tab:> ,trail:-,nbsp:+,lead:•,eol:↲"
 vim.opt.virtualedit = "block"
