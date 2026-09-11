683ca9569f52ef842844674c5fc356ecbda79e89
Christoph Lipp <christoph@lipp.ws>
2026-09-11 10:21:50 +0200

    Add configuration for claude-code.nvim pluginf

---
+++ home/config/nvim/lua/plugins/claude.lua
@@ -0,0 +1,9 @@
+return {
+  "carlos-rodrigo/claude-code.nvim",
+  keys = {
+    { "<leader>cc", "<cmd>ClaudeCodeToggle<cr>", desc = "Toggle Claude Panel" },
+  },
+  opts = {
+    window_type = "vsplit", -- Choose between split, tab, or floating
+  },
+}
